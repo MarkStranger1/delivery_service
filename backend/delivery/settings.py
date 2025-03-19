038cc5d49c1e8ee5c42deeb6bc63c32eb6ab9f96
MarkStranger1 <art225.03@gmail.com>
2025-03-19 20:20:31 +0400

    update port for DB

--- backend/delivery/settings.py
+++ backend/delivery/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER", default="postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
         "HOST": os.getenv("DB_HOST", default="database"),
-        "PORT": os.getenv("DB_PORT", default="5432"),
+        "PORT": 5432,
     },
 }
 # else:
